dcab9d28c3a15401064906ab5165a19840f9b6de
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2022-01-30 19:26:51 +0530

    add toast

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
 
 function ManageCoursePage(props) {
   const [course, setCourse] = useState({
@@ -19,6 +20,7 @@ function ManageCoursePage(props) {
     e.preventDefault();
     courseApi.saveCourse(course).then(() => {
       props.history.push("/courses");
+      toast.success("Course Saved");
     });
   }
 
